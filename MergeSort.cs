572d55f5c8dc3ab8f53ed07e720150abeebfa6a6
mamingrui <2021297569@qq.com>
2021-07-14 10:31:52 +0800

    第一次提交

---
+++ MergeSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructSort
+{
+    /// <summary>
+    /// 二路归并排序
+    /// </summary>
+    class MergeSortDS
+    {
+        private int[] array;                             //待排序序列
+        public MergeSortDS(int[] _arr)
+        {
+            this.array = _arr;
+        }
+        private void MergeSort(int low,int high)
+        {
+            if (low<high)                                //low与high分别是最小索引和最大索引
+            {
+                int mid = (low + high) / 2;
+                MergeSort(low, mid);                     //递归左侧部分
+                MergeSort(mid + 1, high);                //递归右侧部分
+                Merge(low, mid, high);
+            }
+        }
+        //将两个有序序列array[low,....mid]与array[low+1,.....high]合并
+
+        private void Merge(int low,int mid,int high)
+        {
+            //临时空间，存放合并后的数据
+            int[] array1 = new int[high - low + 1];
+            int i = low;                                 //
+            int j = mid + 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace DataStructSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 2, 8, 5, 7, 1, 4};
+            InsertSort(arr);
+            Console.ReadKey();
+        }
+        public static void InsertSort(int[] arr)
+        {
+            for (int i=1;i<arr.Length;i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+                while (j>=0 && temp < arr[j])
+                {
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+                arr[j+1] = temp;
+            }
+            foreach (int item in arr)
+            {
+                Console.Write(item + ",");
+            }
+        }
+        public static void BinarySort(int[] arr)
+        {
+            for (int i=1;i<arr.Length;i++)
+            {
+                int temp = arr[i];
+                int low = 0;
+                int high = i - 1;
+                while (low <= high)
+                {
+                    int mid = (low + high) / 2;
+                    if (temp>arr[mid])
+                    {
+                        high = mid - 1;
+
+
+                    }else if (temp<=arr[mid])
+                    {
+                        low = mid + 1;
+                    }
+                }
+                for (int j=i-1;j>=low;j--)
+                {
+                    arr[j + 1] = arr[j]; 
+                }
+                arr[low] = temp;              
+            }
+        }
+
+        public static void ShellSort(int[] arr)
+        {
+            for (int gap = arr.Length / 2; gap >= 1; gap /= 2)          //步长的循环
+            {
+                for (int i = gap; i < arr.Length; i++)                //该步长下循环
+                {
+                    int temp = arr[i];
+                    int j = i - gap;                            //被交换的元素
+                    while (j >= 0 && temp < arr[j])
+                    {
+                        //交换
+                        arr[j + gap] = arr[j];
+                        j -= gap;
+                    }
+                    arr[j + gap] = temp;
+
+                }
+            }
+            foreach (int item in arr)
+            {
+                Console.Write(item);
+            }
+        }
+        public static void BubbleSort(int[] arr)
+        {
+            int temp = 0;
+            for (int i=0;i<arr.Length-1;i++)
+            {
+                for (int j=0;j<arr.Length-i-1;j++)
+                {
+                    if (arr[j]> arr[j+1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            foreach (int item in arr)
+            {
+                Console.Write(item);
+            }
+        }
+        public static void CockTailSort(int[] arr)
+        {
+
+            for (int i = 1; i < arr.Length / 2; i++)
+            {
+                for(int j = 0; j < arr.Length-1; j++)
+                {
+                    if (arr[j]>arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+                for (int j=arr.Length-i;j>=i;j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = temp;
+                    }
+                }
+            }
+            foreach (int item in arr)
+            {
+                Console.Write(item);
+            }
+        }
+    
+        /// <summary>
+        /// 快速排序
+        /// </summary>
+        /// <param name="arr">待排序的序列</param>
+        /// <param name="low">最小索引</param>
+        /// <param name="high">最大索引</param>
+        static void QuickSort(int[] arr,int low,int high)
+        {
+
+            int i = low;
+            int j = high;                                     //右侧索引号
+            int temp = arr[i];                                //基准值
+            while (i<j)                                       
+            {
+                while (i < j && arr[j] >= temp)
+                {
+                    j--;
+                }
+                arr[i] = arr[j];
+                while (i < j && arr[i] <= temp)
+                {
+                    i++;                                            //从左到右扫描直到找到比基准值大的元素
+                }
+                arr[j] = arr[i];
+            }
+            arr[i] = temp;                                          //基准值归位
+            QuickSort(arr, low, i - 1);                             //递归左侧区间
+            QuickSort(arr,i+1,high);                                //递归右侧区间
+        }
+    }
+}
